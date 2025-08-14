89e0af542c2a3feb714b703847ea711a680399ff
yasmin <makihappy265@gmail.com>
2025-08-14 14:03:56 +0900

    初回コミット

---
+++ src/main/java/raisetech/StudentManagement2/StudentManagement2Application.java
@@ -0,0 +1,22 @@
+package raisetech.StudentManagement2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentManagement2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagement2Application.class, args);
+  }
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello,world!";
+  }
+}
+
+
---
+++ src/test/java/raisetech/StudentManagement2/StudentManagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.StudentManagement2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
