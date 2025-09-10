3bdf1c5eeacdc990d2f283e34e65f39644ec45e9
yasmin <makihappy265@gmail.com>
2025-09-11 03:08:16 +0900

    再コミット

---
+++ src/main/java/raisetech/StudentManagement2/Student.java
@@ -0,0 +1,19 @@
+package raisetech.StudentManagement2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Student {
+
+  private String id;
+  private String name;
+  private String furigana;
+  private String nickname;
+  private String email;
+  private String region;
+  private int age;
+  private String gender;
+}
--- src/main/java/raisetech/StudentManagement2/StudentManagement2Application.java
+++ src/main/java/raisetech/StudentManagement2/StudentManagement2Application.java
@@ -1,6 +1,7 @@
 package raisetech.StudentManagement2;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagement2Application {
 
+  @Autowired
+  private StudentRepository repository;
+
   public static void main(String[] args) {
     SpringApplication.run(StudentManagement2Application.class, args);
+
   }
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "hello,world!";
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return repository.search();
+
+
   }
 }
+
 
 
